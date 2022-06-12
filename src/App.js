38a9f622f5469ed160deb2d52b62ec174defe847
EsraaSaad298 <esraa_mohyeldin@hotmail.com>
2022-06-12 16:36:59 +0400

    updates

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import SocialMedia from './SocialMedia/SocialMedia';
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter  basename={process.env.PUBLIC_URL}>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
